5776ba88e5044031fe952ad50ab677b6b505efb3
xidanyeweiyang <ytliuming47455@sina.com>
2017-03-15 12:03:23 +0800

    上传gif

--- LMNavgationBar/LMNavgationBar/LMNav/UINavigationBar+LMNavigationBar.h
+++ LMNavgationBar/LMNavgationBar/LMNav/UINavigationBar+LMNavigationBar.h
@@ -4,7 +4,9 @@
 //
 //  Created by 刘明 on 17/3/13.
 //  Copyright © 2017年 刘明. All rights reserved.
-//
+//  简书地址:http://www.jianshu.com/p/60668e17ba47
+//  GitHub地址:https://github.com/xidanyeweiyang/LMNavgationBarStyle
+
 
 #import <UIKit/UIKit.h>
 
